d12e5a135e1900b9fba9c844171d57da3be2faf9
Evan Hahn <me@evanhahn.com>
2015-01-06 00:56:36 -0800

    Builtins maybe

---
+++ builtins/which.js
@@ -0,0 +1,52 @@
+const async = require('async');
+const builtins = require('../lib/builtins');
+const config = require('../lib/config');
+const executable = require('executable');
+const fs = require('fs');
+const home = require('home');
+
+function findProgramPath(program, callback) {
+
+  let result = {
+    found: false
+  };
+
+  const paths = [''].concat(config.path);
+  for (let path of paths) {
+    const programPath = home.resolve(path, program);
+    const fileExists = fs.existsSync(programPath);
+    if (fileExists && executable.sync(programPath)) {
+      result.path = programPath;
+      result.found = true;
+      break;
+    }
+  }
+
+  if (!result && builtins[program]) {
+    result.fn = builtins[program];
+    result.isBuiltin = true;
+  }
+
+  callback(null, result);
+
+}
+
+function which(paths, callback) {
+  async.eachSeries(paths, (path, done) => {
+    findProgramPath(path, (err, program) => {
+      if (err) { return done(err); }
+      if (program.path) {
+        console.log(program.path);
+      } else if (program.isBuiltin) {
+        console.log('built into mush');
+      } else if (!program.found) {
+        console.error(path + ' not found');
+      }
+      callback(null, { exitCode: 1 });
+    });
+  }, callback);
+}
+
+which.findProgramPath = findProgramPath;
+
+module.exports = which;
